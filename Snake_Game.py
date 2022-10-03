ec639b7a0d4f3c72ca0b06c3c802f270202f31e5
Activeone/mateo.alvarez <mateo.alvarez@activeone.co>
2022-10-03 18:21:11 -0500

    Ajustes finales

--- Snake_Game.py
+++ Snake_Game.py
@@ -1,70 +1,133 @@
 import pygame
 import random
-from collections import deque
 
 
 class Game:
-    '''
+    """
     The game window and the functions associated with it
-    '''
+    """
 
     bg = (194, 223, 227)
     colorScore = (37, 50, 55)
     colorLost = (37, 50, 55)
+    colorGrid = (117, 134, 144)
 
-    def __init__(self, cantWidth, cantHeight, tamBlock):
-        self.cantWidth = cantWidth
-        self.cantHeight = cantHeight
+    def __init__(self, blocksWidth, blocksHeight, tamBlock):
+        self.blocksWidth = blocksWidth
+        self.blocksHeight = blocksHeight
         self.tamBlock = tamBlock
-        self.window = pygame.display.set_mode((self.cantWidth*self.tamBlock, self.cantHeight*self.tamBlock))
+        self.window = pygame.display.set_mode(
+            (self.blocksWidth * self.tamBlock, self.blocksHeight * self.tamBlock)
+        )
         pygame.display.set_caption("ED - Snake")
-    
+
     def setBackground(self):
-        ''' Revisar '''
         self.window.fill(Game.bg)
 
-    def Your_score(self, score):
-        value = pygame.font.SysFont("bahnschrift", 35).render("SCORE: " + str(score), True, Game.colorScore)
+    def setGrid(self):
+        # Horizontales
+        for i in range(0, self.blocksWidth):
+            pygame.draw.line(
+                self.window,
+                self.colorGrid,
+                (0, i * self.tamBlock),
+                (self.blocksWidth * self.tamBlock, i * self.tamBlock),
+            )
+        # Verticales
+        for i in range(0, self.blocksHeight):
+            pygame.draw.line(
+                self.window,
+                self.colorGrid,
+                (i * self.tamBlock, 0),
+                (i * self.tamBlock, self.blocksHeight * self.tamBlock),
+            )
+
+    def setScore(self, score):
+        value = pygame.font.SysFont("bahnschrift", 35).render(
+            "SCORE: " + str(score), True, Game.colorScore
+        )
         self.window.blit(value, [5, 5])
 
-    def message(self, msg):
+    def setMessage(self, msg):
         mesg = pygame.font.SysFont("bahnschrift", 25).render(msg, True, Game.colorLost)
-        mesg_rect = mesg.get_rect(center=((self.cantWidth*self.tamBlock)/2, (self.cantHeight*self.tamBlock)/2))
+        mesg_rect = mesg.get_rect(
+            center=(
+                (self.blocksWidth * self.tamBlock) / 2,
+                (self.blocksHeight * self.tamBlock) / 2,
+            )
+        )
         self.window.blit(mesg, mesg_rect)
 
     def instructions(self):
-        lines = ["Use the arrow keys to move the snake.", "Eat food to grow. Stay on the board!", "And don't crash into yourself!",'','' ,"To start, please press any arrow key"]
+        lines = [
+            "Use the arrow keys to move the snake.",
+            "Eat food to grow. Stay on the board!",
+            "And don't crash into yourself!",
+            "",
+            "",
+            "To start, please press spacebar key",
+        ]
         for i, l in enumerate(lines):
-            mesg = pygame.font.SysFont("bahnschrift", 25).render(l, True, Game.colorLost)
-            mesg_rect = mesg.get_rect(center=((self.cantWidth*self.tamBlock)/2, (self.cantHeight*self.tamBlock)/2 - 2*self.tamBlock + 25*i))
+            mesg = pygame.font.SysFont("bahnschrift", 25).render(
+                l, True, Game.colorLost
+            )
+            mesg_rect = mesg.get_rect(
+                center=(
+                    (self.blocksWidth * self.tamBlock) / 2,
+                    (self.blocksHeight * self.tamBlock) / 2 - 2 * self.tamBlock + 25 * i,
+                )
+            )
             self.window.blit(mesg, mesg_rect)
 
+
 class Snake:
-    '''
+    """
     The body of the Snake
-    '''
+    """
+
     bg = (50, 153, 213)
     colorBody = (117, 134, 144)
     colorHead = (71, 77, 81)
 
     def __init__(self, window: Game):
         self.window: Game = window
-        self.body = deque()
-        self.body.append((window.cantWidth//2, window.cantHeight//2))
+        self.body = []
+        # Añade cabeza
+        self.body.append((window.blocksWidth // 2, window.blocksHeight // 2 + 1))
+        # Añade 2 bloques de cuerpo
+        self.body.append((window.blocksWidth // 2, window.blocksHeight // 2 + 2))
+        self.body.append((window.blocksWidth // 2, window.blocksHeight // 2 + 3))
         self.dir = 0
         self.changeX = 0
         self.changeY = 0
 
     def drawSnake(self):
-        x = 0
-        pygame.draw.rect(self.window.window, Snake.colorHead, (self.body[0][0]*self.window.tamBlock, self.body[0][1]*self.window.tamBlock, self.window.tamBlock, self.window.tamBlock))
-        for block in self.body:
-            if x == 0:
-                x += 1
-                continue
-
-            pygame.draw.rect(self.window.window, Snake.colorBody, (block[0]*self.window.tamBlock, block[1]*self.window.tamBlock, self.window.tamBlock, self.window.tamBlock))
-        
+        for i in range(len(self.body)):
+            block = self.body[len(self.body) - i - 1]
+            if i == len(self.body) - 1:
+                # Dibuja cabeza
+                pygame.draw.rect(
+                    self.window.window,
+                    Snake.colorHead,
+                    (
+                        self.body[0][0] * self.window.tamBlock,
+                        self.body[0][1] * self.window.tamBlock,
+                        self.window.tamBlock,
+                        self.window.tamBlock,
+                    ),
+                )
+            else:
+                # Dibuja cuerpo
+                pygame.draw.rect(
+                    self.window.window,
+                    Snake.colorBody,
+                    (
+                        block[0] * self.window.tamBlock,
+                        block[1] * self.window.tamBlock,
+                        self.window.tamBlock,
+                        self.window.tamBlock,
+                    ),
+                )
 
     def changeDirection(self, key):
         if key == 1 and self.dir != -1:
@@ -85,22 +148,18 @@ class Snake:
             self.changeY = 0
 
     def move(self):
-        if len(self.body) == 1:
-            block = self.body.pop()
-            self.body.appendleft((block[0]+self.changeX, block[1]+self.changeY))
-        else:
-            self.body.pop()
-            self.body.appendleft((self.body[0][0]+self.changeX, self.body[0][1]+self.changeY))
+        self.body.pop()
+        self.body.insert(0, (self.body[0][0] + self.changeX, self.body[0][1] + self.changeY))
 
     def eat(self):
-        self.body.appendleft((self.body[0][0]+self.changeX, self.body[0][1]+self.changeY))
+        self.body.insert(0, (self.body[0][0] + self.changeX, self.body[0][1] + self.changeY))
 
 
 class Apple:
     red = (213, 50, 80)
 
     def __init__(self, window: Game, snake: Snake):
-        self.appear = random.randint(0,9)
+        self.appear = random.randint(0, 9)
         self.window = window
         self.snake = snake
         self.x = -1
@@ -110,60 +169,57 @@ class Apple:
         if self.appear != 0:
             self.appear -= 1
             return
-        
+
         if self.x == -1:
             self.calPosition()
 
-        pygame.draw.rect(self.window.window, Apple.red, (self.x*self.window.tamBlock, self.y*self.window.tamBlock, self.window.tamBlock, self.window.tamBlock))
+        pygame.draw.rect(
+            self.window.window,
+            Apple.red,
+            (
+                self.x * self.window.tamBlock,
+                self.y * self.window.tamBlock,
+                self.window.tamBlock,
+                self.window.tamBlock,
+            ),
+        )
 
     def inicio(self):
         self.appear = 0
-        self.x = self.snake.body[0][0]+3
-        self.y = self.snake.body[0][1]-3
+        self.x = self.snake.body[0][0] + 3
+        self.y = self.snake.body[0][1] - 4
 
     def calPosition(self):
-        self.x = random.randint(0, self.window.cantWidth-1)
-        self.y = random.randint(0, self.window.cantHeight-1)
+        self.x = random.randint(0, self.window.blocksWidth - 1)
+        self.y = random.randint(0, self.window.blocksHeight - 1)
 
         while True:
-            x = True
-            y = True
-
             for block in self.snake.body:
-                if block[0] == self.x:
-                    self.x = random.randint(0, self.window.cantWidth-1)
-                    x = False
+                if block[0] == self.x and block[1] == self.y:
+                    self.x = random.randint(0, self.window.blocksWidth - 1)
+                    self.y = random.randint(0, self.window.blocksHeight - 1)
+
                     break
-
-            for block in self.snake.body:
-                if block[1] == self.y:
-                    self.y = random.randint(0, self.window.cantHeight-1)
-                    y = False
-                    break
-
-            if not (x == False and y == False):
-                break
-
-        
-
+            break
 
 
 def gameLopp():
     pygame.init()
-    window = Game(13,13,40)
+    window = Game(13, 13, 45)
     snake = Snake(window)
     apple = Apple(window, snake)
     apple.inicio()
     gameClose = False
     gameOver = False
+    instructionsSkipped = False
 
     while not gameClose:
-        
+
         # Player loses
         while gameOver:
             window.setBackground()
-            window.message("You have lost. Please enter 'C' to play again")
-            window.Your_score(len(snake.body) - 1)
+            window.setMessage("You have lost. Please press 'C' key to play again")
+            window.setScore(len(snake.body) - 3)
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -178,7 +234,7 @@ def gameLopp():
                         pygame.quit()
                         gameLopp()
                         return
-        
+
         if gameClose:
             pygame.quit()
             break
@@ -188,8 +244,17 @@ def gameLopp():
             # Quit event
             if event.type == pygame.QUIT:
                 gameClose = True
-                break      
-            if event.type == pygame.KEYDOWN:
+                break
+
+            if (
+                event.type == pygame.KEYDOWN
+                and event.key == pygame.K_SPACE
+                and not instructionsSkipped
+            ):
+                snake.changeDirection(1)
+                instructionsSkipped = True
+                break
+            if event.type == pygame.KEYDOWN and instructionsSkipped:
                 if event.key == pygame.K_LEFT:
                     snake.changeDirection(-2)
                     break
@@ -207,32 +272,15 @@ def gameLopp():
             pygame.quit()
             break
 
-        if snake.dir == 0:
+        if not instructionsSkipped:
             window.setBackground()
             window.instructions()
             pygame.display.update()
             continue
 
-
         window.setBackground()
+        window.setGrid()
 
-        x = snake.body[0][0]
-        y = snake.body[0][1]
-
-        if x >= window.cantWidth or x < 0 or y >= window.cantHeight or y < 0:
-            gameOver = True
-            continue
-        
-        i = 1
-        for block in snake.body:
-            if i == 1:
-                i += 1
-                continue
-
-            if x == block[0] and y == block[1]:
-                gameOver = True
-                break
-        
         if gameOver:
             continue
 
@@ -242,16 +290,30 @@ def gameLopp():
         else:
             snake.move()
 
+        # Revisa si se choca con su cuerpo
+        x = snake.body[0][0]
+        y = snake.body[0][1]
+        for i in range(1, len(snake.body)):
+            block = snake.body[i]
+            if x == block[0] and y == block[1]:
+                gameOver = True
+                break
+        # Revisa si se sale de la pantalla
+        if x >= window.blocksWidth or y < 0 or x < 0 or y >= window.blocksHeight:
+            gameOver = True
+            continue
+
         apple.drawApple()
         snake.drawSnake()
 
-        window.Your_score(len(snake.body) - 1)
+        window.setScore(len(snake.body) - 3)
 
         pygame.display.update()
 
-        pygame.time.delay(100)
-               
+        pygame.time.delay(1000)
+
     pygame.quit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     gameLopp()
